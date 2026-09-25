1134ca08df8b610507f8bd5ae710a150ad54fedb
Kauan Stipp Pedroso <kauan.stipp.pedroso@escola.pr.gov.br>
2026-09-25 11:35:45 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+# Fazer menu
+# Sistema de gerenciamento
+# 
+
+print("=== Bem-vindo ao Sistema de Estoque ===")
+
+opcao = input("Escolha uma opção:") 
+
+
+print("1 - Cadastrar peças")
+print("2 - Remover peças")
+print("3 - Retirar peça do estoque")
+print("4 - Consultar peças")
+print("5 - Sair")
+
+while True:
+    opcao = input("Escolha uma opção:")
